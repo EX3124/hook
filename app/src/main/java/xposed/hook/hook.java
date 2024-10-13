1b363af9fca9a8b88c4d115b60d9004435538ac3
EX3124 <70867298+EX3124@users.noreply.github.com>
2024-10-13 20:58:11 +0800

    1

--- app/src/main/java/xposed/hook/hook.java
+++ app/src/main/java/xposed/hook/hook.java
@@ -1,19 +1,26 @@
 package xposed.hook;
 
+import android.content.Intent;
+
 import androidx.annotation.Keep;
 
 import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
 
 @Keep
 public class hook implements IXposedHookLoadPackage {
     @Override
     public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) {
-        if (lpparam.packageName.equals("com.xposedscope"))
-            mainhook(lpparam);
-    }
-
-    public void mainhook(XC_LoadPackage.LoadPackageParam lpparam) {
-
+        XposedHelpers.findAndHookMethod("android.app.Activity", lpparam.classLoader, "startActivity", Intent.class, new XC_MethodHook() {
+            @Override
+            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                super.beforeHookedMethod(param);
+                Intent intent = (Intent) param.args[0];
+                intent.setComponent(null);
+                param.args[0] = Intent.createChooser(intent, "");
+            }
+        });
     }
 }
