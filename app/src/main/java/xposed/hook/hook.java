29effe153f687653e86ef3c52744d3794aa2fe96
EX3124 <70867298+EX3124@users.noreply.github.com>
2024-10-14 23:22:19 +0800

    3

--- app/src/main/java/xposed/hook/hook.java
+++ app/src/main/java/xposed/hook/hook.java
@@ -18,23 +18,24 @@ public class hook implements IXposedHookLoadPackage {
             @Override
             protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
                 super.beforeHookedMethod(param);
-                Intent intent = (Intent) param.args[0];
-                if (intent.getData() != null & intent.getData().isAbsolute()) {
-                    intent.setComponent(null);
-                    param.args[0] = Intent.createChooser(intent, "");
-                }
+                param.args[0] = mainhook(param);
             }
         });
-        XposedHelpers.findAndHookMethod("android.content.Context", lpparam.classLoader, "startActivity", Intent.class, Bundle.class, new XC_MethodHook() {
+        XposedHelpers.findAndHookMethod("android.app.Activity", lpparam.classLoader, "startActivity", Intent.class, Bundle.class, new XC_MethodHook() {
             @Override
             protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
                 super.beforeHookedMethod(param);
-                Intent intent = (Intent) param.args[0];
-                if (intent.getData() != null & intent.getData().isAbsolute()) {
-                    intent.setComponent(null);
-                    param.args[0] = Intent.createChooser(intent, "");
-                }
+                param.args[0] = mainhook(param);
             }
         });
+    }
+
+    public Object mainhook(XC_MethodHook.MethodHookParam param) {
+        Intent intent = (Intent) param.args[0];
+        if (intent.getData() != null) {
+            intent.setComponent(null);
+            param.args[0] = Intent.createChooser(intent, "");
+        }
+        return param.args[0];
     }
 }
